4ffe243d1fe64d94f0f90383df50b402a5056c12
orianapedroza <pedrozapalomar.ov07@gmail.com>
2026-06-18 22:09:52 -0400

    new notebooks

--- sourceRE_BKT/models/__init__.py
+++ sourceRE_BKT/models/__init__.py
@@ -5,7 +5,6 @@ from .emotion_hybrid.re_bkt import REBKT as REBKT_Hybrid
 
 __all__ = ['REBKT_Internal', 'REBKT_External', 'REBKT_Hybrid']
 
-# Importación limpia gracias a los __init__.py
 #from models import REBKT_Internal, REBKT_External, REBKT_Hybrid
 #from fit.model_base import ModelBase #
 
